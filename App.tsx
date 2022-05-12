c4f070311577e87e6faaac7451ef7b0f9510373b
daniell0l <daniel_l0l@hotmail.com>
2022-05-12 17:28:25 -0300

    api-mobile

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Inter_500Medium,
+  Inter_400Regular,
+} from "@expo-google-fonts/inter";
+
+import { theme } from "./src/theme";
+import { Widget } from "./src/components/Widget";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_500Medium,
+    Inter_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <Widget />
+
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+    </View>
+  );
+}
---
+++ src/components/Widget/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper'
+
+export const styles = StyleSheet.create({
+  button: {
+    width: 48,
+    height: 48,
+    borderRadius: 24
+  }
+});
